e585727b8ed13c8f94d902f9a25502af5f10cc17
Jaime1401 <jaime14albalte@gmail.com>
2022-10-11 12:28:34 +0200

    lab2 done

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,12 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+
+    implementation("org.springdoc:springdoc-openapi-data-rest:1.6.0")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.0")
+    implementation("org.springdoc:springdoc-openapi-kotlin:1.6.0")
+
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
